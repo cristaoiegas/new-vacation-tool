92eb49b0aa9fb858fce724bc99dfd6e2f40c5200
cristaoiegas <crista.oiegas@evozon.com>
2014-05-16 11:45:41 +0300

    z

---
+++ resource/Constants.java
@@ -0,0 +1,10 @@
+package com.requirements.tools;
+
+public class Constants {
+public static String user_pm = "pmDOI";
+public static String password_pm = "pmDOI";
+public static String user_dm = "dmDOI";
+public static String password_dm = "dmDOI";
+public static String user_usual = "crista.oiegas";
+public static String password_usual = "parolatest";
+}
